e588f7e919ea319bad78801a05676bd5f38f24ad
HotMeltedCheese <145945277+HotMeltedCheese@users.noreply.github.com>
2024-12-19 12:00:32 -1000

    Changed constant to all caps. Swerve teleop control changed

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
+import javax.management.InstanceNotFoundException;
+
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.SerialPort.StopBits;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.autos.*;
 import frc.robot.commands.*;
-import frc.robot.commands.PhotonVisionCmds.RotateMove;
 import frc.robot.subsystems.*;
 //import frc.robot.subsystems.SysIDTest;
 
@@ -23,41 +25,37 @@ import frc.robot.subsystems.*;
  */
 public class RobotContainer {
     /* Controllers */
-    private final Joystick driver = new Joystick(0);
-    private final CommandXboxController driver2 = new CommandXboxController(2);
+    // private final Joystick driver = new Joystick(0);
+    private final CommandXboxController driver0 = new CommandXboxController(0);
+    private final CommandXboxController driver1 = new CommandXboxController(1);
 
     /* Drive Controls */
-    private final int translationAxis = XboxController.Axis.kLeftY.value;
-    private final int strafeAxis = XboxController.Axis.kLeftX.value;
-    private final int rotationAxis = XboxController.Axis.kRightX.value;
+    // private final int translationAxis = XboxController.Axis.kLeftY.value;
+    // private final int strafeAxis = XboxController.Axis.kLeftX.value;
+    // private final int rotationAxis = XboxController.Axis.kRightX.value;
     
     /* Driver Buttons */
-    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
-    private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
-
-    private final Timer timer = new Timer();
+    // private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
+    // private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
 
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
-    private final SysIDTest testingSwerve = new SysIDTest(s_Swerve);
-    private final PhotonVision camera1 = new PhotonVision(null);
-   // private final SysIDTest testingSwerve = new SysIDTest(s_Swerve);
-
-   //Commands
-   private final RotateMove c_DriveToTag = new RotateMove(null,null,null,null,null);
 
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
-        s_Swerve.setDefaultCommand(
-            new TeleopSwerve(
-                s_Swerve, 
-                () -> -driver.getRawAxis(translationAxis), 
-                () -> -driver.getRawAxis(strafeAxis), 
-                () -> -driver.getRawAxis(rotationAxis), 
-                () -> robotCentric.getAsBoolean()
-            )
-        );
+
+        s_Swerve.setDefaultCommand(new TeleopSwerve(
+            s_Swerve, 
+            () -> -driver0.getLeftY(), 
+            () -> -driver0.getLeftX(), 
+            () -> -driver0.getRightX(),
+            () -> driver0.y().getAsBoolean()));
         // Configure the button bindings
+
+        // driver0.a().toggleOnTrue( new angle(s_Swerve, 79));
+        // driver0.a().toggleOnFalse(new angle(s_Swerve, 20));
+
+        // driver0.b().toggleOnTrue( new angle(s_Swerve, 40));
         configureButtonBindings();
     }
 
@@ -69,24 +67,6 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         /* Driver Buttons */
-        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
-        //configureTestBindings();
-    }
-
-    private void configureTestBindings() {
-        driver2.a().onTrue(testingSwerve.sysIdDynamic(SysIdRoutine.Direction.kForward));
-        driver2.b().onTrue(testingSwerve.sysIdDynamic(SysIdRoutine.Direction.kForward));
-        driver2.x().onTrue(testingSwerve.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-        driver2.y().onTrue(testingSwerve.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    }
-
-    /**
-     * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // An ExampleCommand will run in autonomous
-        return new exampleAuto(s_Swerve);
+        driver0.rightBumper().onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
     }
 }
